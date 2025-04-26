6c2f800422750e13c060cab4de94404347b24baa
aleksmn <al-mn@yandex.ru>
2025-04-26 15:51:25 +0300

    add second alarm in 00 minutes

--- script.js
+++ script.js
@@ -7,54 +7,53 @@ function startHourlyTimer() {
         const now = new Date();
         const currentMinutes = now.getMinutes();
         const currentSeconds = now.getSeconds();
+        const currentHours = now.getHours();
+
+        // Устанавливаем время на 45 минут текущего часа
+        const signalTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), currentHours, 45, 0, 0);
+        const nextHourSignalTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), currentHours + 1, 0, 0, 0);
 
         // Проверяем, если текущее время XX:00
         if (currentMinutes === 0 && currentSeconds === 0) {
-            // Устанавливаем время на 45 минут следующего часа
-            const nextSignalTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours() + 1, 45, 0, 0);
-            signal();
-            // Устанавливаем таймер на следующее срабатывание
-            setTimeout(() => {
-                signal();
-                setInterval(signal, 60 * 60 * 1000); // 1 час
-            }, nextSignalTime - now);
-        } else {
-            // Обновляем таймер
-            const nextSignalTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), 45, 0, 0);
-            if (now > nextSignalTime) {
-                nextSignalTime.setHours(nextSignalTime.getHours() + 1);
-            }
-            updateTimer(nextSignalTime);
+            signal(false); // Воспроизводим сигнал alarm2.wav
+        } else if (now >= signalTime && now < nextHourSignalTime) {
+            // Если текущее время между чч:45 и чч:59
+            document.getElementById('timer').innerHTML = "Время!";
+            document.title = "Время!";
+            // Смена иконки
+            document.getElementById('favicon').href = "images/success-icon.png"
+        } else if (now < signalTime) {
+            // Смена иконки
+            document.getElementById('favicon').href = "images/icon.png"
+            // Если текущее время до чч:45
+            const timeDiff = signalTime - now;
+            updateTimer(timeDiff);
         }
     }, 1000);
 }
 
+function updateTimer(timeDiff) {
+    const minutes = padNumber(Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60)));
+    const seconds = padNumber(Math.floor((timeDiff % (1000 * 60)) / 1000));
 
-function updateTimer(targetTime) {
-    const now = new Date();
-    const timeDiff = targetTime - now;
-
-    if (timeDiff > 0) {
-        const minutes = padNumber(Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60)));
-        const seconds = padNumber(Math.floor((timeDiff % (1000 * 60)) / 1000));
-
-        document.getElementById('timer').innerHTML = `Осталось времени до следующего сигнала: ${minutes}:${seconds}`;
-        // Меняем заголовок окна
-        document.title = `${minutes}:${seconds}`
-    } else {
-        document.getElementById('timer').innerHTML = "Сигнал уже сработал!";
-        document.title = "Время!"
-    }
+    document.getElementById('timer').innerHTML = `Осталось времени до сигнала: ${minutes}:${seconds}`;
+    document.title = `${minutes}:${seconds}`;
 }
 
-function signal() {
+function signal(isStartOfHour) {
+    const alertSound1 = document.getElementById('alertSound1');
+    const alertSound2 = document.getElementById('alertSound2');
 
-    // Воспроизведение звукового сигнала
-    const alertSound = document.getElementById('alertSound');
-    alertSound.play();
+    if (isStartOfHour) {
+        alertSound2.play(); // Воспроизводим сигнал alarm2.wav
+    } else {
+        alertSound1.play(); // Воспроизводим сигнал alarm1.wav
+    }
 
-    // Смена иконки
-    document.getElementById('favicon').href = "images/success-icon.png"
+    // Вывод сообщения о сигнале
+    const messageDiv = document.getElementById('message');
+    const currentTime = new Date().toLocaleTimeString();
+    messageDiv.innerHTML = `Сигнал в 45 минут! Текущее время: ${currentTime}`;
 }
 
 // Запускаем таймер
